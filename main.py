c3989926d8185d642708bbc46ec61d961b2e32ef
taiChantarungsee <tai.jantarungsee@gmail.com>
2017-09-07 22:02:32 +0100

    Refactored code and changed to get Flask working. Can start the server using python or python 3. If that doesn't work than use the temp project or start it as a linux executable

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import json, logging, time, mysql.connector
+#!/usr/bin/env python
+import json, logging, time
+#mysql.connector
 
 from flask import Flask
 from watson_developer_cloud import ToneAnalyzerV3
-from save import insert_data
+#from save import insert_data
 
 """Configure logging file. The program logs exceptions to an, external file, which
 will be useful when scaled. Logging.ERROR indicates the program has not been able to carry out its function."""
@@ -30,12 +32,12 @@ and password. This way of connecting will have to refactored for production use,
   "password": "ec0wKSjOyZb1"
 }"""
 
-try:
-	database = mysql.connector.connect(user='ENTER YOUR USERNAME HERE', password='ENTER YOUR PASSWORD HERE',
-	database='SKU')
-except:
-	print('Database connection failed.')
-	logging.exception(time.strftime("%c")+ ". " + 'Database connection failed. Traceback:')
+#try:
+#	database = mysql.connector.connect(user='ENTER YOUR USERNAME HERE', password='ENTER YOUR PASSWORD HERE',
+#	database='SKU')
+#except:
+#	print('Database connection failed.')
+#	logging.exception(time.strftime("%c")+ ". " + 'Database connection failed. Traceback:')
 
 payload = {
 		'name': 'S12T-MMM-RS',
@@ -44,21 +46,24 @@ payload = {
 
 #Put all the above in a separate file? And don't forget to not include or clean up the log files first.
 
-@app.route('/SKU-Retriever/api/v1.0/retrieve', methods=['POST'])
+@app.route('/SKU-Retriever/api/v1.0/retrieve', methods=['GET'])
 def update_sku():
 	try:
 		print("SKU number is:" + " " + payload['name'])
-		print(json.dumps(tone_analyzer.tone(text=payload['comment']), indent=2))
-		response = json.dumps(tone_analyzer.tone(text=payload['comment']))
+		response = tone_analyzer.tone(text=payload['comment'])
 		#insert_data(payload)
 		print("Comment analyzed and saved sucessfully!")
 	except KeyError as e: #expand on this bit and error handling, tests for all modules and so on tomorrow.
 			print ('No SKU found. Error code:', e)
 			#log the error
 			logging.exception(time.strftime("%c")+ ". " + 'No SKU found. Traceback:')
-			return 400
-	return 201
+			return
+	return jsonify(response)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-	app.run(debug=True)
 	print("Server has started!")
+	app.run(debug=True)
