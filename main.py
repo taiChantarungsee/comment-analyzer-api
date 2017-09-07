6284dbda6c156eea7b34011b04d07a260072d3e9
taiChantarungsee <tai.jantarungsee@gmail.com>
2017-09-07 16:47:16 +0100

    Fixed main function.

--- main.py
+++ main.py
@@ -14,16 +14,22 @@ logging.basicConfig(filename='log.txt', level=logging.ERROR)
 #Set password and options first
 try :
 	tone_analyzer = ToneAnalyzerV3(
-	username='',
-	password='',
-	version='')
+	username='5b55cac9-1576-4669-ac69-c1d8358c0015',
+	password='ec0wKSjOyZb1',
+	version='2016-09-20')
 except:
 	print('Tone analyzer credentials failed.')
 	logging.exception(time.strftime("%c")+ ". " + 'Tone analyzer credentials failed. Traceback:')
 
 """Set up database connections. Program uses a mysql database to store comments, choosen for its reliability.
 Please run the db.py script to set up this database in Mysql, and replace the parameters below with your username
-and password. This way of connecting will have to refactored for production use, but will work for development."""
+and password. This way of connecting will have to refactored for production use, but will work for development.
+{
+  "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+  "username": "5b55cac9-1576-4669-ac69-c1d8358c0015",
+  "password": "ec0wKSjOyZb1"
+}"""
+
 try:
 	database = mysql.connector.connect(user='ENTER YOUR USERNAME HERE', password='ENTER YOUR PASSWORD HERE',
 	database='SKU')
@@ -36,15 +42,15 @@ payload = {
 		'comment': 'I wish I was happy...really!',
 	}
 
-#Put all the above in a separate file?
+#Put all the above in a separate file? And don't forget to not include or clean up the log files first.
 
 @app.route('/SKU-Retriever/api/v1.0/retrieve', methods=['POST'])
-def get_sku():
+def update_sku():
 	try:
 		print("SKU number is:" + " " + payload['name'])
 		print(json.dumps(tone_analyzer.tone(text=payload['comment']), indent=2))
 		response = json.dumps(tone_analyzer.tone(text=payload['comment']))
-		insert_data(payload)
+		#insert_data(payload)
 		print("Comment analyzed and saved sucessfully!")
 	except KeyError as e: #expand on this bit and error handling, tests for all modules and so on tomorrow.
 			print ('No SKU found. Error code:', e)
@@ -54,4 +60,5 @@ def get_sku():
 	return 201
 
 if __name__ == '__main__':
-	main(payload)
+	app.run(debug=True)
+	print("Server has started!")
